64697575798b3f379a2dabe77b4acec317baa8d0
NeneeMalo <117932797+NeneeMalo@users.noreply.github.com>
2022-11-16 16:56:32 -0500

    Subida

---
+++ src/main/java/pe/com/EmpresaCanario/EmpresaCanarioApplication.java
@@ -0,0 +1,13 @@
+package pe.com.EmpresaCanario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresaCanarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpresaCanarioApplication.class, args);
+	}
+
+}
