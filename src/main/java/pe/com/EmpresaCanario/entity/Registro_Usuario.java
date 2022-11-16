95ab1fd8adf77a9fe5b51c54b6603ff22294bb2c
JuanAlbornoz <juanleotak@gmail.com>
2022-11-16 17:21:14 -0500

    Cliente y Registro_Usuario

--- src/main/java/pe/com/EmpresaCanario/entity/Registro_Usuario.java
+++ src/main/java/pe/com/EmpresaCanario/entity/Registro_Usuario.java
@@ -6,29 +6,40 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-@Entity(name="Cliente")
-@Table(name="cliente")
-public class Cliente implements Serializable {
+@Entity(name="Registro_Usuario")
+@Table(name="registro_usuario")
+public class Registro_Usuario implements Serializable {
     private static final long serialVersionUID=1L;
     @Id
-    @Column(name="id_cliente")
+    @Column(name="id_registro")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long idcliente;
-    @ManyToOne
-    @JoinColumn(name="id_registro", nullable=false)
-    private Registro_Usuario registrousuario;
+    private long idregistro;
+    @Column(name="nombre")
+    private String nombre;
+    @Column(name="apellido")
+    private String apellido;
+    @Column(name="dni")
+    private String dni;
+    @Column(name="correo")
+    private String correo;
+    @Column(name="telefono")
+    private String telefono;
+    @Column(name="direccion")
+    private String direccion;
+    @Column(name="contrasena")
+    private String contrasena;
     @Column(name="estado")
     private boolean estado;
+    
 }
